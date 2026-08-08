2f09bb6ca45c0b66c1d24fe44cb236d7385ef0f9
oussama <sarbouteoussama@gmail.com>
2026-08-08 20:25:46 +0100

    added some new tables

--- interface/appointments/models.py
+++ interface/appointments/models.py
@@ -1,3 +1,108 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Doctor(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    email = models.EmailField(unique=True)
+
+    specialization = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Dr. {self.first_name} {self.last_name}"
+
+
+
+class Patient(models.Model):
+
+    GENDER_CHOICES = [
+        ("M", "Male"),
+        ("F", "Female"),
+    ]
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    birth_date = models.DateField()
+
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES
+    )
+
+    phone = models.CharField(
+        max_length=20
+    )
+
+    def __str__(self):
+        return self.user.username
+
+
+
+class TimeSlot(models.Model):
+
+    doctor = models.ForeignKey(
+        Doctor,
+        on_delete=models.CASCADE,
+        related_name="slots"
+    )
+
+    date = models.DateField()
+
+    start_time = models.TimeField()
+
+    end_time = models.TimeField()
+
+    def __str__(self):
+        return (
+            f"{self.doctor} "
+            f"{self.date} "
+            f"{self.start_time}"
+        )
+
+
+
+class Appointment(models.Model):
+
+    STATUS_CHOICES = [
+        ("PENDING", "Pending"),
+        ("CONFIRMED", "Confirmed"),
+        ("COMPLETED", "Completed"),
+        ("CANCELLED", "Cancelled"),
+        ("NO_SHOW", "No Show"),
+    ]
+
+    patient = models.ForeignKey(
+        Patient,
+        on_delete=models.CASCADE,
+        related_name="appointments"
+    )
+
+    slot = models.OneToOneField(
+        TimeSlot,
+        on_delete=models.CASCADE
+    )
+
+    booking_date = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    status = models.CharField(
+        max_length=15,
+        choices=STATUS_CHOICES,
+        default="PENDING"
+    )
+
+    predicted_no_show = models.FloatField(
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return (
+            f"{self.patient} - {self.slot}"
+        )
