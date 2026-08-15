66516250750aec8f343f936190bbbb747ab03485
oussama <sarbouteoussama@gmail.com>
2026-08-15 20:01:15 +0100

    replacing the django standard unittest with pytest

---
+++ interface/accounts/tests/test_auth.py
@@ -0,0 +1,39 @@
+import pytest
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
+from appointments.models import Patient
+
+
+@pytest.mark.django_db
+def test_dashboard_requires_login(client):
+
+    response = client.get(reverse("dashboard"))
+
+    assert response.status_code == 302
+
+
+
+@pytest.mark.django_db
+def test_dashboard_authenticated(client):
+
+    user = User.objects.create_user(
+        username="patient",
+        password="StrongPassword123",
+    )
+
+    Patient.objects.create(
+        user=user,
+        birth_date=date(2000, 1, 1),
+        gender="M",
+        phone="0611111111",
+    )
+
+    client.login(
+        username="patient",
+        password="StrongPassword123",
+    )
+
+    response = client.get(reverse("dashboard"))
+
+    assert response.status_code == 200
